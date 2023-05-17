9a8e2b148d743c588bea61891cd111a91b32c132
Enstryl <machinchivu@gmail.com>
2023-05-17 11:06:49 +0200

    milieu tp partie2

--- src/components/FicheEpisode.vue
+++ src/components/FicheEpisode.vue
@@ -1,9 +1,47 @@
 <script setup>
+import {useRoute} from "vue-router";
+import {onMounted, ref} from "vue";
+const route = useRoute();
+const episode = ref();
+const id = route.params.id
+const personnage = ref();
+const infosperso = ref();
 
+onMounted(async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/episode/${id}`);
+    episode.value = await response.json();
+
+
+    // Affichage de l'url des personnages
+    // personnage.value = episode.value.characters;
+    // personnage.value = await episode.value.map((personnage) => {
+    //     let fin = personnage.split('/').pop()
+    //     return personnage.replace(personnage, fin)
+    // })
+})
 </script>
 
 <template>
   <main>
+      <pre>
+        {{personnage}}
+    </pre>
+      <div v-if="episode != null">
+          Nom : {{episode.name}} <br>
+          Episode : {{episode.episode}} <br>
+          Sortie : {{episode.air_date}} <br>
+<!--          <template v-for="personnage in personnages">-->
+<!--              <router-link :to="{ name: 'fichePerso', params: { id: personnage }}">Personnage {{personnage}}</router-link>-->
+<!--              <br>-->
+<!--          </template>-->
+
+
+      </div>
+      <div v-else>
+          Chargement en cours ...
+      </div>
+      <router-link to="/">Retour aux épisodes</router-link>
+
   </main>
 </template>
 
--- src/views/EpisodesView.vue
+++ src/views/EpisodesView.vue
@@ -5,6 +5,8 @@ import  {onMounted, ref } from 'vue';
 const episodes = ref([]);
 const pagineSuivante = ref('');
 const paginePrecedente = ref('');
+const filtreDate = ref('');
+const filtreNumero = ref('');
 
 onMounted(async () => {
     const response = await fetch('https://rickandmortyapi.com/api/episode');
@@ -27,6 +29,22 @@ async function previousPage(){
     pagineSuivante.value = await episodes.value.info.next;
     paginePrecedente.value = await episodes.value.info.prev;
 }
+async function filterByDate(){
+  const response = await fetch('https://rickandmortyapi.com/api/episode?air_date');
+  episodes.value = await response.json();
+  pagineSuivante.value = await episodes.value.info.next;
+  paginePrecedente.value = await episodes.value.info.prev;
+}
+async function filterByNumber(){
+  const response = await fetch('https://rickandmortyapi.com/api/episode?episode');
+  episodes.value = await response.json();
+  pagineSuivante.value = await episodes.value.info.next;
+  paginePrecedente.value = await episodes.value.info.prev;
+
+}
+
+
+
 </script>
 
 
@@ -38,11 +56,19 @@ async function previousPage(){
 <!--</pre>-->
 
     <template class="flex">
-        <div v-for="episode in episodes.results">
-            <div class="card">
+        <div class="filtrage">
+            <p>Filtrer par : </p>
+            <button @click="filtreByDate">Date</button>
+            <button @click="filtreByNumber">Numéro</button>
+        </div>
+        <div class="flex">
+            <div v-for="episode in episodes.results">
+                <div class="card">
                 Nom : {{episode.name}} <br>
-                Date de sortie : {{episode.air_date}}
+                Date de sortie : {{episode.air_date}} <br>
+                Numéro : {{episode.episode}}
                 <router-link :to="{ name: 'ficheEpisode', params: { id: episode.id }}">Voir plus</router-link>
+                 </div>
             </div>
         </div>
     </template>
